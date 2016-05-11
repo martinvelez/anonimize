f4ee064eacb97752b088c211afc70c5775a81e26
Martin Velez <mvelez999@gmail.com>
2016-05-11 16:26:44 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='anonimize',
+			version='0.0.0',
+			description='Anonimize files, directories, and git repos',
+			author='Martin Velez, Casey Casalnuovo, Sahana Mundewadi',
+			author_email='mvelez999@gmail.com',
+			url='',
+			packages=['distutils', 'distutils.command'],
+			)
