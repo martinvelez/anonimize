e76551ea5a12bb7f533f632c1cb7d98ae2b1ae06
Andrew Mendoza <aalmendoza@ucdavis.edu>
2016-09-14 12:27:42 -0700

    Skip files with invalid encoding

--- anonimize/anonimize.py
+++ anonimize/anonimize.py
@@ -14,19 +14,22 @@ def comment_remover(text):
 	return os.linesep.join([s for s in r1.splitlines() if s.strip()])
 
 def write_anonymized_file(infile, outfile):
-	root, ext = os.path.splitext(infile)
-	valid = [".c", ".h", "Makefile"]
-	if ext.lower() in valid:
-		inf = open(infile, "r")
-		dirty = inf.read()
-		clean = comment_remover(dirty)
-		inf.close()
-		outf = open(outfile, "w") 
-		outf.write(clean)
-		outf.close()
-		print("Comments are removed in following:", infile, ">>>", outfile) #and this
-	else:
-		print("File not valid:     ", infile)
+	try:
+		root, ext = os.path.splitext(infile)
+		valid = [".c", ".h", "Makefile"]
+		if ext.lower() in valid:
+			inf = open(infile, "r")
+			dirty = inf.read()
+			clean = comment_remover(dirty)
+			inf.close()
+			outf = open(outfile, "w") 
+			outf.write(clean)
+			outf.close()
+			print("Comments are removed in following:", infile, ">>>", outfile) #and this
+		else:
+			print("File not valid:     ", infile)
+	except UnicodeDecodeError:
+		print("Can't remove comments in: ", infile)
 
 def make_dir_for_anonymized_files(target_dir):
 	timeStamp =  datetime.fromtimestamp(time.time()).strftime("%m-%d-%y-%H:%M:%S")
